f39a8de9da1b113dd7fb305f171f5281b5431abd
Daniel Aguilar <dacmail@gmail.com>
2017-02-02 16:56:11 +0100

    quitada licencia

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 	</footer>
     <div class="copy">
         <div class="container">
-            &copy; Ayuntamiento de Madrid CC by
+            &copy; Ayuntamiento de Madrid
             <?php wp_nav_menu(array(
                         'container' => 'span',
                         'items_wrap' => '%3$s',
@@ -32,7 +32,7 @@
                         'walker' => new ungrynerd_walker_nav_menu));
             ?>
             <div class="pull-right">
-                 <a href="https://www.facebook.com/pages/Ayuntamiento-de-Madrid" target="_blank">facebook</a>,
+                <a href="https://www.facebook.com/pages/Ayuntamiento-de-Madrid" target="_blank">facebook</a>,
                 <a href="http://instagram.com/madrid" target="_blank">instagram</a>,
                 <a href="http://twitter.com/Madrid" target="_blank">twitter</a>,
                 <a href="https://www.youtube.com/channel/UCYY0va5t-KZncOOctoGva7A" target="_blank">youtube</a>
