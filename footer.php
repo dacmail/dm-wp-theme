7d7d60d9c2b86936e47a96daa89f9d57cd4d10a6
Daniel Aguilar <dacmail@gmail.com>
2017-02-07 11:46:55 +0100

    Codigo analytics

--- footer.php
+++ footer.php
@@ -40,5 +40,20 @@
         </div>
     </div>
 	<?php wp_footer(); ?>
+    <!-- Piwik -->
+    <script type="text/javascript">
+        var _paq = _paq || [];
+    // tracker methods like "setCustomDimension" should be called before "trackPageView"
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//webanalytics01.madrid.es/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '8']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+    </script>
+    <!-- End Piwik Code -->
 </body>
 </html>
