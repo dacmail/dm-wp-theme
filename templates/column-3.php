64c9bbbe94cd4a158bfa0c811e7599420a0a7481
Daniel Aguilar <dacmail@gmail.com>
2017-02-14 11:13:26 +0100

    Footer y posts destacados de portada

--- templates/column-3.php
+++ templates/column-3.php
@@ -1,19 +1,21 @@
-<?php
-    $current_blog = get_current_blog_id();
-    switch_to_blog($site_id);
-    $post_block = new WP_Query(array('p' => $selected_post, 'post_type' => array('post')));
-?>
-<?php if ( $post_block->have_posts() ) : while ( $post_block->have_posts() ) : $post_block->the_post(); ?>
-    <article <?php post_class('column-3 post-home post-column') ?>>
-        <?php the_blog_title($current_blog); ?>
-        <?php the_first_tag(); ?>
-        <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <div class="post-author"><?php the_gallery_icon(); ?><strong><?php the_time(get_option('date_format')); ?></strong></div>
-        <div class="post-content">
-            <?php the_excerpt(); ?>
-        </div>
-        <?php get_template_part('templates/post-share') ?>
-    </article>
-<?php endwhile; ?>
+<?php if (!empty($selected_post)): ?>
+    <?php
+        $current_blog = get_current_blog_id();
+        switch_to_blog($site_id);
+        $post_block = new WP_Query(array('p' => $selected_post, 'post_type' => array('post')));
+    ?>
+    <?php if ( $post_block->have_posts() ) : while ( $post_block->have_posts() ) : $post_block->the_post(); ?>
+        <article <?php post_class('column-3 post-home post-column') ?>>
+            <?php the_blog_title($current_blog); ?>
+            <?php the_first_tag(); ?>
+            <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div class="post-author"><?php the_gallery_icon(); ?><strong><?php the_time(get_option('date_format')); ?></strong></div>
+            <div class="post-content">
+                <?php the_excerpt(); ?>
+            </div>
+            <?php get_template_part('templates/post-share') ?>
+        </article>
+    <?php endwhile; ?>
+    <?php endif; ?>
+    <?php switch_to_blog($current_blog); ?>
 <?php endif; ?>
-<?php switch_to_blog($current_blog); ?>
--- templates/follow-us.php
+++ templates/follow-us.php
@@ -1,6 +1,6 @@
 <div class="follow-us">
     <span>Síguenos</span>
-    <a href="https://www.facebook.com/pages/Ayuntamiento-de-Madrid" target="_blank"><i class="fa fa-facebook"></i></a>
+    <a href="https://www.facebook.com/ayuntamientodemadrid/" target="_blank"><i class="fa fa-facebook"></i></a>
     <a href="http://instagram.com/madrid" target="_blank"><i class="fa fa-instagram"></i></a>
     <a href="http://twitter.com/Madrid" target="_blank"><i class="fa fa-twitter"></i></a>
     <a href="https://www.youtube.com/channel/UCYY0va5t-KZncOOctoGva7A" target="_blank"><i class="fa fa-youtube-square"></i></a>
