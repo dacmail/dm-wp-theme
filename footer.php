5785dfb642ae5c91031420f40be1c46b7be4cd07
Daniel Aguilar <dacmail@gmail.com>
2024-08-01 10:46:13 +0200

    Eliminación Matomo

--- footer.php
+++ footer.php
@@ -40,21 +40,6 @@
         </div>
     </div>
 	<?php wp_footer(); ?>
-    <!-- Piwik -->
-    <script type="text/javascript">
-        var _paq = _paq || [];
-    // tracker methods like "setCustomDimension" should be called before "trackPageView"
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-        var u="//webanalytics01.madrid.es/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', '8']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-    })();
-    </script>
-    <!-- End Piwik Code -->
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136636213-1"></script>
     <script>
